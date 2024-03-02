4106032b6ea105394f4eda9169eec7d85122d44f
Zaheerkhan07 <113497115+Zaheerkhan07@users.noreply.github.com>
2024-03-02 10:59:46 +0530

    Add files via upload

---
+++ sqrt.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int num,root;
+	printf("enter the number:");
+	scanf("%d",&num);
+	
+	root=sqrt(num);
+	printf("sqrt of number is %d:%d",num,root);
+	
+}
